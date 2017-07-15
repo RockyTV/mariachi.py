2ff1aeb62201fa47d31dfcc0bf8625fc0c3de6c9
Alexandre Oliveira <rockytvbr@gmail.com>
2017-07-15 13:37:30 -0300

    Update module loading code
    Instead of importing each command class then adding it to a list, it is
    now possible to only add the class name to the list and automatically
    import it.

--- mariachipy/app.py
+++ mariachipy/app.py
@@ -5,7 +5,7 @@ It contains the definition of routes and views for the application.
 import os
 import time
 import telepot
-import commands
+import importlib
 from flask import Flask, request
 from telepot.loop import MessageLoop
 
@@ -19,13 +19,21 @@ registered_commands = []
 
 def register_commands():
     import types
+    to_register = ['Ping', 'Notes']
+
+    def import_from(module, name):
+        module = importlib.import_module(module)
+        return getattr(module, name)
+
     def register_command(cmd: types.ModuleType):
         try:
             registered_commands.index(cmd)
         except ValueError:
             registered_commands.append(cmd(bot))
+            print('Registered command module:', cmd.__name__)
 
-    register_command(commands.Ping)
+    for command in to_register:
+        register_command(import_from('commands', command))
 
 def on_chat_message(msg):
     print(msg)
